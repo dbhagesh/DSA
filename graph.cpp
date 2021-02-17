cdc13dc1041fd42b0510b375ae6260aec4c6ed47
Bhagesh <dbhagesh@gmail.com>
2021-02-17 22:46:45 +0530

    Updated DFS

--- graph.cpp
+++ graph.cpp
@@ -1,23 +1,103 @@
-#include<list>
+
 #include<bits/stdc++.h>
 
 typedef long long int ll;
 using namespace std;
-class graph{
-    vector<vector<int>> adjList;
+class Graph{
+    vector<vector<pair<int,int>>> adjList;
+    //Edges
+    int n;
+
 public:
-    void func(){
-        adjList.push_back(1);
+    Graph(int N){
+        n=N;
+        adjList.resize(n,vector<pair<int,int>>());
+
     }
-    void printg(){
-        for(auto i: adjList) cout<<i<<" ";
+    void addNewEdge(int u, int v, int d=1){
+        auto p1 = make_pair(v,d);
+        auto p2 = make_pair(u,d);
+        adjList[u].push_back(p1);
+        adjList[v].push_back(p2);
+    }
+    void printAdjacencyList(){
+        cout<<"Adjacency List of Graph\n";
+        for(int i=0; i<n; i++){
+            cout<<i<<"->";
+            for(auto j: adjList[i]){
+                cout<<"["<<j.first<<","<<j.second<<"]";
+            }
+            cout<<'\n';
+        }
+    }
+
+    void breadthFirstTraversal(int src){
+        cout<<"BFS traversal"<<endl;
+        queue<int> q;
+
+        int vis[n]={0};
+        q.push(src);
+        vis[src]=1;
+        while(!q.empty()){
+
+            int cur=q.front();
+            cout<<cur<<",";
+            q.pop();
+
+            for(auto i:adjList[cur]){
+                if(vis[i.first]==0)
+                    q.push(i.first);
+                vis[i.first]=1;
+            }
+        }
+
+
+    }
+    void dfshelper(int ele, unordered_map<int,bool> &vis){
+
+        vis[ele]=true;
+        cout<<ele<<",";
+
+        for(auto i: adjList[ele]){
+
+            if(!vis[i.first])
+                dfshelper(i.first,vis);
+        }
+    }
+
+    void dfs(){
+        unordered_map<int,bool> vis;
+        for(int i=0; i<n; i++)
+            vis[i]=false;
+        cout<<"DFS traversal"<<endl;
+        dfshelper(0,vis);
+
+
     }
 };
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    graph obj;
-    obj.func();
-    obj.printg();
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    Graph obj(5);
+    obj.addNewEdge(0,1);
+    obj.addNewEdge(0,3);
+    obj.addNewEdge(0,4);
+    obj.addNewEdge(1,2);
+    obj.addNewEdge(1,3);
+    obj.addNewEdge(2,3);
+    obj.addNewEdge(4,3);
+    obj.printAdjacencyList();
+    obj.breadthFirstTraversal(0);
+    cout<<endl;
+    obj.dfs();
+
+    return 0;
+
 }
 
--- p.cpp
+++ p.cpp
@@ -1,8 +1,76 @@
+
 #include<bits/stdc++.h>
+
+typedef long long int ll;
 using namespace std;
-int arr[10];
-int main(){
-    int arr[10];
-    for(int i=0; i<10; i++)
-        cout<<arr[i]<<endl;
+
+const int MAX = 1e6+5;
+
+bool prime[MAX];
+int spf[MAX];
+
+void sieve()
+{
+    fill(prime, prime + MAX, true);
+    for (int i = 1; i < MAX; i++)spf[i] = i;
+    prime[0] = prime[1] = false;
+    for (int i = 2; i * i < MAX; i++) {
+        if (prime[i]) {
+            for (int j = i * i; j < MAX; j += i) {
+                if (prime[j]) spf[j] = i;
+                prime[j] = false;
+            }
+        }
+    }
 }
+
+//Multiple Query
+vector<int> getFactorization(int x)
+{
+    vector<int> ret;
+    while (x != 1)
+    {
+        ret.push_back(spf[x]);
+        x = x / spf[x];
+    }
+    return ret;
+}
+
+
+int main()
+{
+
+
+
+	sieve();
+
+
+
+
+
+
+	int t,n;
+	cin>>t;
+	while(t--){
+		cin>>n;
+        int dp[n];
+        dp[0]=dp[1]=0;
+        int ans=0;
+        for(int i=2; i<(n); i++){
+
+
+            if(prime[i] and prime[i-2]) ans++;
+                dp[i]=ans;
+
+
+
+	}
+
+		cout<<dp[n]<<'\n';
+
+	}
+
+	return 0;
+
+}
+
